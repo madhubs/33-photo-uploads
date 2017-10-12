863219c9eda55e734e589c409e4eb2e2f0f60c30
Madeline Stevens <stevensm1987@gmail.com>
2017-10-12 15:45:59 -0700

    just adding the starter code for this lab

--- lab-maddy/sluggram-frontend/src/component/navbar/index.js
+++ lab-maddy/sluggram-frontend/src/component/navbar/index.js
@@ -2,16 +2,29 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
-import {tokenDelete} from '../../action/auth-action';
+import {tokenDelete} from '../../action/auth-actions';
 
 class Navbar extends React.Component {
   render() {
     return (
       <header>
+        {this.props.auth && this.props.profile ?
+          <div className="profile-header">
+            <h2>Welcome {this.props.profile.username}</h2>
+            <img src={this.props.profile.avatar} style={{'width': '15%', 'border': '1px solid grey'}}/>
+          </div>
+          :
+          undefined
+        }
         <nav>
           <ul>
             {this.props.auth ?
-              <li onClick={this.props.tokenDelete}><Link to="/">Logout</Link></li> :
+              <div>
+                <li onClick={this.props.tokenDelete}><Link to="/">Logout</Link></li>
+                <li><Link to="/dashboard">Dashboard</Link></li>
+                <li><Link to="/settings">Settings</Link></li>
+              </div>
+              :
               <div>
                 <li><Link to="/welcome/signup">Signup</Link></li>
                 <li><Link to="/welcome/login">Login</Link></li>
@@ -26,6 +39,7 @@ class Navbar extends React.Component {
 
 let mapStateToProps = state => ({
   auth: state.auth,
+  profile: state.profile,
 });
 
 let mapDispatchToProps = dispatch => ({
